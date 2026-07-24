0dcb907ae6fedde6a1e5ec21861c07f49b4a9681
Juan Daniel Pulido <juandanielpulidovargas@gmail.com>
2026-07-24 11:39:43 -0500

    Configuracion de variables de entorno

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (_req, res) => {
-   res.send('Aprendiendo express, ficha 3407181');
+   res.send('Aprendiendo express, con la ficha 3407181 y con un homesexual llamado Torres.');
 });
 
 app.listen(port, () => {
