dcdcd2c0ffa79f1e131dd6668b0793e178147b5b
Juan Daniel Pulido <juandanielpulidovargas@gmail.com>
2026-07-24 10:17:21 -0500

    feat: inicializar proyecto express a mi nombre

---
+++ app.js
@@ -0,0 +1,11 @@
+import express from 'express';
+const app = express();
+const port = 3000;
+
+app.get('/', (_req, res) => {
+   res.send('Aprendiendo express, ficha 3407181');
+});
+
+app.listen(port, () => {
+   console.log('Servidor en funcionamiento en el puerto: ' + port);
+});
