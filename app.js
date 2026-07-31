bb962e9013576f97f571b334aa19e60bd4074f74
Daniel <Daniel@example.com>
2026-07-31 10:23:36 -0500

    aplicacion configurado las variables de entorno

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 import express from 'express';
+import {configDotenv} from "dotenv" 
+configDotenv()
+
 const app = express();
-const port = process.env.PUERTO || 3000
+const port = process.env.PUERTO || 5050
 
 app.get('/', (_req, res) => {
-   res.send('Aprendiendo express, con la ficha 3407181');
+   res.send(`Aprendiendo express, con la ficha 3407181, ADSO en el Sena`);
 });
+
+
 
 app.listen(port, () => {
    console.log('Servidor en funcionamiento en el puerto: ' + port);
