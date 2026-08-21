17bd96adea79fe6544829c5f91b827907a9dece9
Steve <tu_correo@ejemplo.com>
2026-08-21 11:27:03 -0500

    Primer endpint para listar aprendices

--- app.js
+++ app.js
@@ -3,9 +3,23 @@ const app = express()
 require("dotenv/config")
 const port = process.env.puerto
 const PUERTO = port || 3030
+const sistemaArchivo = require("fs")
+const ruta= require("path")
+const rutaArchivoJson = ruta.join(__dirname, "datos.json")
 
 app.get("/", function (req, res) {
    res.send("API - Rest Aprendices");
+})
+//
+app.get("/api/aprendices", function (req, res) {
+   //los datos se pueden traer de una base de datos, de un archivo, etc
+   sistemaArchivo.readFile(rutaArchivoJson, "utf8", function(error, datos) {
+      if (error) {
+         return res.json({Error: "No se pudo leer los datos"})
+      }
+      const listaAprendices = JSON.parse(datos);
+      res.json(listaAprendices);
+   })
 })
 
 app.listen(PUERTO, function() {
